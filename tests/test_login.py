3513d4a679d8f649ad25be59603446893f609734
prasanna.yalluru <pyalluru@gmail.com>
2026-03-02 18:48:34 +0530

    Automated test_001

--- tests/test_login.py
+++ tests/test_login.py
@@ -9,3 +9,12 @@ class TestLogin:
         self.login_page = LoginPage(driver)
         self.driver = driver
 
+# Verify login with wrong username & wrong password
+    def test_002(self, driver):
+        self.login_page.click_pom()
+        self.login_page.set_user_name("aaa")
+        self.login_page.set_password("aaa")
+        self.login_page.click_login_button()
+        # Capture error message
+        assert self.login_page.is_alert_present() == True
+        assert self.login_page.alert_message_text() == '⚠️ Invalid username or password. Please try again.'
